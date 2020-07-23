2033794fc201f0f947d6fcbc3e90eee68f11f3ae
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-07-23 13:28:47 +0000

    Development of checking if sequence matches user selections. Issues in correct matching still evident. Working progress.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,11 +6,13 @@ When the user selects the difficulty and clicks start:
 
 - Generate Sequence with Math Random Y
 - Iterate through sequence and apply highlight function Y
--May need to use a promise to let highlights be seperate  Y
+- May need to use a promise to let highlights be seperate  Y
 - Wait for sequence to end Y
 - Allow user to select options Y
-- Run check function to compare selections with current game sequence
+- Run check function to compare selections with current game sequence ********
 -If Correct. Continue.
+- RESTART BUTTON Y
+- START BUTTON Y
 If Incorrect start from begining 
 */
 
@@ -26,7 +28,11 @@ const Rookie = 2000;
 const Veteran = 1200;
 const MVP = 800;
 
+const roundLimit = 10;
 const playOrder = [kick, pass, run, rush];
+
+var selectionMade = false;
+var answerCorrect = false;
 
 var currentGame = [];
 var difficulty = Veteran;
@@ -42,27 +48,35 @@ var playType;
 const restart = () => {
   clicked = [];
   round = 1;
-  lives = 3;
+  lives = 2;
   randomOrder = [];
+  currentGame = [];
   makeGameSequence();
 };
+
+// const getRandomPlay = card => {
+//     let card = playOrder[parseInt(Math.random * playOrder.length)];
+//     return card;
+
+// }
 
 //-------- CREATES A NEW AND RANDOMISED CURRENT GAME LIST---BASED ON ROUND (each round increases by one)-----//
 
 let makeGameSequence = () => {
-//   for (let x = 0; x < round; x++) {
-    var y = Math.round(Math.random() * (playOrder.length - 1));
-    currentGame.push(playOrder[y]);
-//   }
-  console.log(currentGame); //---DEVELOPMENT VIEW
+  var y = Math.round(Math.random() * (playOrder.length - 1));
+  currentGame.push(playOrder[y]);
   for (i in currentGame) {
     playTypeObject = currentGame[i];
     playTypeId = playTypeObject.getAttribute("data-id");
     randomOrder.push(playTypeId);
     i++;
   }
+  console.log(currentGame);
+  console.log(randomOrder);
 };
+
 makeGameSequence();
+
 // ------- FLASH FUNCTION INITIATES HIGHLIGHT ON CARD----//
 
 const flash = (card) => {
@@ -73,7 +87,7 @@ const flash = (card) => {
       setTimeout(() => {
         //-----allows for repeating playtypes in the code so there is a gap between flashes-----
         resolve();
-      }, 400);
+      }, 500);
     }, difficulty);
   });
 };
@@ -82,36 +96,73 @@ $("#play-btn").click(function () {
   main();
 });
 
-const cardClick = (card) => {
-  playType = card.getAttribute("data-id");
-  console.log(playType);
-  clicked.push(playType);
-  let choice = clicked.shift();
-  let correctAnswer = randomOrder.shift();
-  if (lives > 0) {
-    if (clicked.length === randomOrder.length) {
-      if (choice === correctAnswer) {
-        clicked = [];
-        alert(`You completed round ${round}!`);
-        randomOrder = [];
-        round += 1;
-        makeGameSequence();
-      } else if (choice !== correctAnswer) {
-        alert("INCORRECT. YOU LOSE 1 LIFE!");
-        clicked = [];
-        currentGame = currentGame;
-        lives -= 1;
-      }
-    } else {
-      console.log("Make Selection!");
-    }
-  } else {
-      alert("OUT OF LIVES! GAME OVER!")
-  }
-};
-//----------main function iterates through currentGame (randomised sequence) and flashes each card within
+//----------Play button initiates main function which iterates through currentGame (randomised sequence) and flashes each card
 const main = async () => {
+  canClick = false;
   for (let card of currentGame) {
     await flash(card);
+    canClick = true;
   }
 };
+
+//------Function to check answers with selections--------
+// const checkAnswer = () => {
+//   var choice = clicked[0];
+//   var answer = randomOrder[0];
+//   if (lives > 0) {
+//     if (clicked.length === randomOrder.length) {
+//       if (choice === answer) {
+//         clicked.shift();
+//         alert(`You completed round ${round}!`);
+//         round += 1;
+//         makeGameSequence();
+//       } else {
+//         alert("INCORRECT. YOU LOSE 1 LIFE!");
+//         clicked = [];
+//         lives -= 1;
+//       }
+//     } else {
+//       console.log("Select another...");
+//     }
+//   } else {
+//     alert("GAME OVER!");
+//   }
+//   //   }
+
+//---------CARD CLICK FUNCTION RETRIEVES DATA ID OF CARD CLICKED AND INITIATES CHECK ANSWER FUNCTION -------
+
+const cardClick = (card) => {
+  playType = card.getAttribute("data-id");
+  clicked.push(playType);
+  let selectedCards = [...clicked];
+  let sequence = [...randomOrder];
+  if (selectedCards.length === sequence.length) {
+    var choice = selectedCards.shift();
+    var answer = sequence.shift();
+    if (choice === answer) {
+      randomOrder.shift();
+      console.log("correct");
+      alert(`You completed round ${round}!`);
+      round += 1;
+      makeGameSequence();
+    } else if (choice !== answer) {
+      console.log("INCORRECT. YOU LOSE 1 LIFE!");
+      clicked = [];
+      lives -= 1;
+    }
+  } else {
+    console.log("select more");
+  }
+};
+
+// while (clicked.length < currentGame.length) {
+//   selectionMade = false;
+// }
+
+// while (clicked.length === currentGame.length) {
+//   selectionMade = true;
+// }
+
+// if ((selectionMade = true)) {
+//   checkAnswer();
+// }
