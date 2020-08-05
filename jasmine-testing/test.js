1764c24bf4be6e44136b753fb396cb1c066129c0
Daniel Bradford <danielbradford@hotmail.co.uk>
2020-08-05 14:10:09 +0000

    More specs/ tests added to jasmine test.js file

--- jasmine-testing/test.js
+++ jasmine-testing/test.js
@@ -1,3 +1,5 @@
+
+//-------AUTOMATED CHECK THAT OF INITIAL GAME SETTINGS ON PAGE LOAD--------
 describe("Check base settings for game", function () {
   describe("Life Check", function () {
     it("Starting lives should be set at 3", function () {
@@ -16,8 +18,20 @@ describe("Check base settings for game", function () {
       expect(points).toBe(0);
     });
   });
+  describe("Current Game", function () {
+    it("Current game array should should be empty", function () {
+      expect(currentGame).toEqual([]);
+    });
+  });
+  describe("Random Order", function () {
+    it("Random Order array should should be empty", function () {
+      expect(currentGame).toEqual([]);
+    });
+  });
+
 });
 
+//-------AUTOMATED CHECK THAT RESTART BUTTON RESETS ALL COUNTERS-------
 describe("Restart button functions for game", function () {
   describe("Restart Button", function () {
     it("should reset lives", function () {
@@ -32,6 +46,26 @@ describe("Restart button functions for game", function () {
       restart();
       expect(round).toBe(1);
     });
+    it("should reset clicked array", function () {
+      restart();
+      expect(clicked).toEqual([]);
+    });
+    it("should reset currentGame array", function () {
+      restart();
+      expect(currentGame).toEqual([]);
+    });
+    it("should reset randomOrder array", function () {
+      restart();
+      expect(randomOrder).toEqual([]);
+    });
   });
 });
 
+
+//-------AUTOMATED CHECK OF DEFAULT DIFFICULTY-----
+describe("Check difficulty", function () {
+    it("should be set to default of rookie", function (){
+        checkDifficulty();
+        expect(difficulty).toEqual(1200);
+    })
+});
